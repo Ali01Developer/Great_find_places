ac5e79e308f1f5bbdd5a4528746b3c9468ff1b5f
inamhwebelight <114744326+inamhwebelight@users.noreply.github.com>
2022-10-03 10:21:01 +0530

    Update great_places.dart

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -19,7 +19,7 @@ class GreatPlaces with ChangeNotifier {
       id: DateTime.now().toString(),
       title: title,
       location: PlaceLocation(
-        latitude: latitude,
+        
         longitude: longitude,
         addess: address,
       ),
@@ -37,6 +37,21 @@ class GreatPlaces with ChangeNotifier {
       'loc_lng': newPlace.location?.longitude ?? 0.0,
       'address': newPlace.location?.addess ?? ""
     });
+     DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location?.latitude ?? 0.0,
+      'loc_lng': newPlace.location?.longitude ?? 0.0,
+      'address': newPlace.location?.addess ?? ""
+    }); DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location?.latitude ?? 0.0,
+      'loc_lng': newPlace.location?.longitude ?? 0.0,
+      'address': newPlace.location?.addess ?? ""
+    });
   }
 
   Future<void> getAndSetPlaces() async {
@@ -44,7 +59,7 @@ class GreatPlaces with ChangeNotifier {
     _items = placesList.map((place) {
       return Place(
         id: place['id'],
-        title: place['title'],
+       
         image: File(place['image']),
         location: PlaceLocation(
           latitude: place['loc_lat'],
@@ -53,11 +68,19 @@ class GreatPlaces with ChangeNotifier {
         ),
       );
     }).toList();
+     DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location?.latitude ?? 0.0,
+      'loc_lng': newPlace.location?.longitude ?? 0.0,
+      'address': newPlace.location?.addess ?? ""
+    });
 
     notifyListeners();
   }
 
   Place findById(String id) {
-    return _items.firstWhere((element) => id == element.id);
+    return _items.firstWhere((element) => id == element);
   }
 }
